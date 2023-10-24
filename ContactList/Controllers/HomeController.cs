f75d136a660c78424e2574b343dd06168f750782
snkrs-buz-dood <marvingandhiofficial@gmail.com>
2023-10-24 00:00:23 -0500

    Add project files.

---
+++ ContactList/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ContactList.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ContactListApp;
+
+namespace ContactList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DataDTO dataObj;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var data = dataObj.ConcateData(id);
+            ViewBag.contactValue = data;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
